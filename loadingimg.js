aba20cebe0284291962e88e8a57ed8f3d723ba13
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-03 04:17:36 -0400

    Create loadingimg.js

---
+++ loadingimg.js
@@ -0,0 +1,5 @@
+      var loading = document.createElement('img');
+      loading.id = "loading";
+      loading.src = "https://i.imgur.com/llF5iyg.gif"
+      loading.style = "height: 48px;width: 48px;margin-left: calc(50% - 24px);display:block"
+      document.getElementById("audioClipsContainer").appendChild(loading);
