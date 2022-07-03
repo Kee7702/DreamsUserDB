287e887ada63bc851171e03b11497d8646f28c2d
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-03 04:21:34 -0400

    Update FreakZiIIA8EK6vP3zHfN.js

--- user/FreakZiIIA8EK6vP3zHfN.js
+++ user/FreakZiIIA8EK6vP3zHfN.js
@@ -1,4 +1,7 @@
 document.getElementById('audioImports').innerHTML = "Audio Imports";
+var script = document.createElement('script');
+script.src = "/loadingimg.js";
+document.getElementsByTagName('head')[0].appendChild(script);
 ids.imp = "https://cdn.indreams.me/064eb4e5a0fec49042aea43eb9b492bf_5"
 ids.username = "FreakZiIIA8"
 ids.importCount = "13"
