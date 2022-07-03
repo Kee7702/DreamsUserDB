ca76cdc6f79fe8bb6a9589effa91821c754ed296
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-03 04:42:28 -0400

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,19 +1,13 @@
       var clipname = document.createElement('a');
-      clipname.style = "font-size:16px;line-height:20px;color:#ccc;opacity:0;pointer-events:none";
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
       clipname.className = "global-post-label";
-      clipname.textContent = "loading"
-      clipname.href = "https://indreams.me/element/oMCgjuuYsjs"
+      clipname.textContent = "Loading Imports"
       var clipstatus = document.createElement('a');
-      clipstatus.style = "opacity:0;pointer-events:none"
       clipstatus.className = "global-post-icon";
-      clipstatus.textContent = "loading"
-      var clipdesc = document.createElement('a');
-      clipdesc.style = "opacity:0;pointer-events:none"
-      clipdesc.className = "global-post-details";
-      clipdesc.textContent = "loading"
+      clipstatus.textContent = "loop"
       var clipdiv = document.createElement('div');
       clipdiv.className = "global-annoucement";
+      clipdiv.id = "loading";
       clipdiv.appendChild(clipname);
       clipdiv.appendChild(clipstatus);
-      clipdiv.appendChild(clipdesc);
       document.getElementById("audioClipsContainer").appendChild(clipdiv);
