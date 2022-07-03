e777ea51142f9b4de558955394bff74a5dd0f9db
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 22:05:27 -0400

    Update 6.js

--- user/FreakZiIIA8EK6vP3zHfN/imports/6.js
+++ user/FreakZiIIA8EK6vP3zHfN/imports/6.js
@@ -16,5 +16,5 @@
       clipdiv.appendChild(clipdesc);
       document.getElementById("audioClips").appendChild(clipdiv);
 var script = document.createElement('script');
-script.src = "/Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/5.js";
+script.src = "/user/FreakZiIIA8EK6vP3zHfN/imports/5.js";
 document.getElementsByTagName('head')[0].appendChild(script);
