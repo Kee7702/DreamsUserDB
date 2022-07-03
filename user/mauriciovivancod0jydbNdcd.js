0fe036c358288d29e07dde46b2448ef883987b56
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 22:45:30 -0400

    Create mauriciovivancod0jydbNdcd.js

---
+++ user/mauriciovivancod0jydbNdcd.js
@@ -0,0 +1,13 @@
+document.getElementById('audioImports').innerHTML = "Loading Audio Imports...";
+ids.imp = "https://cdn.indreams.me/c448c1b006615cf77572817af86607e1_5"
+ids.username = "mauriciovivanco"
+ids.importCount = "10"
+ids.importLimit = "Infinite"
+ids.importTime = "Unknown"
+ids.userID = "1"
+var script = document.createElement('script');
+script.src = "/elements.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+var script = document.createElement('script');
+script.src = "/user/FreakZiIIA8EK6vP3zHfN/imports/0.js";
+document.getElementsByTagName('head')[0].appendChild(script);
