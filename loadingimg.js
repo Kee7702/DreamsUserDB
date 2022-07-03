fd6cb5c559ba8c1e63a7ff6029d1494d20670316
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-03 04:32:17 -0400

    Update loadingimg.js

--- loadingimg.js
+++ loadingimg.js
@@ -1,5 +1,19 @@
-      var loading = document.createElement('img');
-      loading.id = "loading";
-      loading.src = "https://i.imgur.com/llF5iyg.gif"
-      loading.style = "height: 48px;width: 48px;margin-left: calc(50% - 24px);display:block"
-      document.getElementById("audioClipsContainer").appendChild(loading);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc;opacity:0;pointer-events:none";
+      clipname.className = "global-post-label";
+      clipname.textContent = "loading"
+      clipname.href = "https://indreams.me/element/oMCgjuuYsjs"
+      var clipstatus = document.createElement('a');
+      clipstatus.style = "opacity:0;pointer-events:none"
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "loading"
+      var clipdesc = document.createElement('a');
+      clipdesc.style = "opacity:0;pointer-events:none"
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "loading"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClipsContainer").appendChild(clipdiv);
