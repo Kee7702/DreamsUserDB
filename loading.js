e9f156e9b509eee53455ca0f58d2eb7a55b7f5a8
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-03 05:00:08 -0400

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,8 +1,9 @@
       var clipname = document.createElement('a');
-      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc;pointer-events:none";
       clipname.className = "global-post-label";
       clipname.textContent = "Loading Imports"
       var clipstatus = document.createElement('a');
+      clipstatus.style = "pointer-events:none"
       clipstatus.className = "global-post-icon";
       clipstatus.textContent = "loop"
       var clipdiv = document.createElement('div');
@@ -10,4 +11,5 @@
       clipdiv.id = "loading";
       clipdiv.appendChild(clipname);
       clipdiv.appendChild(clipstatus);
-      document.getElementById("audioClipsContainer").appendChild(clipdiv);
+      var clipchild = document.getElementById("audioClips").firstChild;
+      document.getElementById("audioClips").insertBefore(clipdiv, clipchild)
