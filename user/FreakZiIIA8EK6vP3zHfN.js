78891b3aeab50194084d3c86507c293debad3ed6
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-06 14:24:02 -0400

    Update FreakZiIIA8EK6vP3zHfN.js

--- user/FreakZiIIA8EK6vP3zHfN.js
+++ user/FreakZiIIA8EK6vP3zHfN.js
@@ -4,14 +4,15 @@ script.src = "/loading.js";
 document.getElementsByTagName('head')[0].appendChild(script);
 ids.imp = "https://cdn.indreams.me/064eb4e5a0fec49042aea43eb9b492bf_5"
 ids.username = "FreakZiIIA8"
-ids.importCount = "13"
+ids.importCount = "12"
 ids.importLimit = "8:10"
 ids.importTime = "2:10"
 ids.userID = "2"
 ids.maxRequestsY = "216"
+ids.pendingRequests = "2"
 var script = document.createElement('script');
 script.src = "/elements.js";
 document.getElementsByTagName('head')[0].appendChild(script);
 var script = document.createElement('script');
-script.src = "/user/FreakZiIIA8EK6vP3zHfN/imports/12.js";
+script.src = "/user/FreakZiIIA8EK6vP3zHfN/imports/14.js";
 document.getElementsByTagName('head')[0].appendChild(script);
