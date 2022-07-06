178d467a6669cc495161e89f45abb3fbe28e3384
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-06 02:13:09 -0400

    Update Despicable_KeePASSPHRASE.js

--- user/Despicable_KeePASSPHRASE.js
+++ user/Despicable_KeePASSPHRASE.js
@@ -4,16 +4,6 @@ script.src = "/loading.js";
 document.getElementsByTagName('head')[0].appendChild(script);
 ids.imp = "https://cdn.indreams.me/39ba5515c2b82cf5d347d4b25de503d3_5"
 ids.username = "Despicable_Kee"
-ids.importCount = "0"
-ids.importLimit = "Infinite"
-ids.importTime = "Unknown"
-ids.userID = "0"
-ids.maxRequestsD = "Infinite"
-ids.maxRequestsW = "Infinite"
-ids.maxRequestsM = "Infinite"
-ids.maxRequestsY = "Infinite"
-ids.pendingLimit = "Infinite"
-ids.pendingRequests = "0"
 var script = document.createElement('script');
 script.src = "/elements.js";
 document.getElementsByTagName('head')[0].appendChild(script);
