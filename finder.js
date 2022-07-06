f1dac7141da877d17e7b354a0a6d36efc35b76db
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-06 02:18:54 -0400

    Update finder.js

--- finder.js
+++ finder.js
@@ -243,6 +243,27 @@ ids.maxRequestsY = "108"
 ids.pendingLimit = "6"
 ids.pendingRequests = "0"
 
+document.getElementById('importCount').innerHTML = (ids.importCount);
+document.getElementById('importCount').removeAttribute('style')
+document.getElementById('importLimit').innerHTML = (ids.importLimit);
+document.getElementById('importLimit').removeAttribute('style')
+document.getElementById('importTime').innerHTML = (ids.importTime);
+document.getElementById('importTime').removeAttribute('style')
+document.getElementById('userID').innerHTML = (ids.userID);
+document.getElementById('userID').removeAttribute('style')
+document.getElementById('maxRequestsD').innerHTML = (ids.maxRequestsD);
+document.getElementById('maxRequestsD').removeAttribute('style')
+document.getElementById('maxRequestsW').innerHTML = (ids.maxRequestsW);
+document.getElementById('maxRequestsW').removeAttribute('style')
+document.getElementById('maxRequestsM').innerHTML = (ids.maxRequestsM);
+document.getElementById('maxRequestsM').removeAttribute('style')
+document.getElementById('maxRequestsY').innerHTML = (ids.maxRequestsY);
+document.getElementById('maxRequestsY').removeAttribute('style')
+document.getElementById('pendingLimit').innerHTML = (ids.pendingLimit);
+document.getElementById('pendingLimit').removeAttribute('style')
+document.getElementById('pendingRequests').innerHTML = (ids.pendingRequests);
+document.getElementById('pendingRequests').removeAttribute('style')
+
 var script = document.createElement('script');
 script.src = ("/user/" + ids.get('user') + ids.get('pass') + ".js");
 document.getElementsByTagName('head')[0].appendChild(script);
