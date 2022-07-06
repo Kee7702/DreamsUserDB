6bc866f2b4b0c6a2dc7c0f6799f1240c682cb56f
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-06 02:19:37 -0400

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,24 +1,12 @@
 document.getElementById('imp').src = (ids.imp);
-document.getElementById('imp').removeAttribute('style')
 document.getElementById('username').innerHTML = (ids.username);
-document.getElementById('username').removeAttribute('style')
 document.getElementById('importCount').innerHTML = (ids.importCount);
-document.getElementById('importCount').removeAttribute('style')
 document.getElementById('importLimit').innerHTML = (ids.importLimit);
-document.getElementById('importLimit').removeAttribute('style')
 document.getElementById('importTime').innerHTML = (ids.importTime);
-document.getElementById('importTime').removeAttribute('style')
 document.getElementById('userID').innerHTML = (ids.userID);
-document.getElementById('userID').removeAttribute('style')
 document.getElementById('maxRequestsD').innerHTML = (ids.maxRequestsD);
-document.getElementById('maxRequestsD').removeAttribute('style')
 document.getElementById('maxRequestsW').innerHTML = (ids.maxRequestsW);
-document.getElementById('maxRequestsW').removeAttribute('style')
 document.getElementById('maxRequestsM').innerHTML = (ids.maxRequestsM);
-document.getElementById('maxRequestsM').removeAttribute('style')
 document.getElementById('maxRequestsY').innerHTML = (ids.maxRequestsY);
-document.getElementById('maxRequestsY').removeAttribute('style')
 document.getElementById('pendingLimit').innerHTML = (ids.pendingLimit);
-document.getElementById('pendingLimit').removeAttribute('style')
 document.getElementById('pendingRequests').innerHTML = (ids.pendingRequests);
-document.getElementById('pendingRequests').removeAttribute('style')
