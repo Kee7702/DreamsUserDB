3ac3d89c8d3180bffb009f19158e217f3213b348
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-12 00:56:43 -0400

    Update finder.js

--- finder.js
+++ finder.js
@@ -298,11 +298,7 @@ do{imports++
       clipdiv.appendChild(clipname);
       clipdiv.appendChild(clipstatus);
       clipdiv.appendChild(clipdesc);
-      if (importlist[imports].hasFile && !importlist[imports].batchedImport) {var importaudio = new Audio('/user/audio/' + importlist[imports].creation_id + ".mp3");
-      importaudio.style = 'width: -webkit-fill-available;height: 20px;margin: 0;display: block;margin-top: 6px';
-      importaudio.controls = true;
-      clipdiv.appendChild(importaudio);}else{}
-      if (importlist[imports].hasFile && importlist[imports].batchedImport) {var importaudio = new Audio('/user/audio/' + importlist[imports].creation_id + importlist[imports].batchedImport + ".mp3");
+      if (importlist[imports].googleID) {var importaudio = new Audio('https://drive.google.com/uc?confirm=t&export=download&id=' + importlist[imports].googleID);
       importaudio.style = 'width: -webkit-fill-available;height: 20px;margin: 0;display: block;margin-top: 6px';
       importaudio.controls = true;
       clipdiv.appendChild(importaudio);}else{}
