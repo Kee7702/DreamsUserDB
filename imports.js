600d7fed74f9aa8103a22de224c5b2d9f16bf2c3
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-13 22:29:16 -0400

    Update imports.js

--- imports.js
+++ imports.js
@@ -39,4 +39,4 @@ do{imports++
       document.getElementById("audioClips").appendChild(clipdiv);
       const players = Array.from(document.getElementsByTagName('audio')).map((p) => new Plyr(p, {controls:['play-large', 'play', 'progress', 'current-time', 'duration', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen', 'download'],settings:['captions', 'quality', 'speed', 'loop'],toggleInvert:false,volume:0.75,invertTime:false,storage:false,loop:{active:true}}));
 console.log=(importlist[imports])}
-while(importlist[imports+1]);})()
+while((imports - 1) % 50 !== 0 && importlist[imports+1]);})()
