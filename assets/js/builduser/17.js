b1b604e1f7bd829a6371f41f9aa2a45cffa135f3
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-09-02 23:09:48 -0400

    Create 17.js

---
+++ assets/js/builduser/17.js
@@ -0,0 +1,13 @@
+(async () => {
+  (iddb = await (await fetch("/data/u/ids")).json());
+  (async () => {
+    (userlist = await (await fetch("/data/u/list")).json());
+    (async () => {
+      (importlist = await (await fetch("/data/i/list")).json());
+        (async () => {
+          (importlist17 = await (await fetch("/data/u/c/17")).json())[0].username = userlist[17].username, userlist[17].impstored && (importlist17[0].impstored=true), importlist17[0].imphash = userlist[17].imphash, imports = 0;
+          do imports++, 17 == importlist[imports].userID && (importlist17[importlist17.length] = importlist[imports]), !importlist[imports+1] && (src=document.createElement('script'),src.type="module",src.textContent=`import { Octokit } from "https://cdn.skypack.dev/@octokit/core";const octokit = new Octokit({auth:document.cookie.split("; ").find(a=>a.startsWith("auth="))?.split("=")[1],});const { data: { sha } } = await octokit.request('GET /repos/{owner}/{repo}/contents/{file_path}', {owner: "Kee7702",repo: "DreamsUserDB",file_path: "data/u/17"});const { data } = await octokit.request('PUT /repos/{owner}/{repo}/contents/{file_path}', {message: new Date().valueOf().toString(36)+' '+importlist17[0].username+' Build', owner: 'Kee7702', sha:{ sha }.sha, repo: 'DreamsUserDB', file_path:'data/u/17',  content: btoa(JSON.stringify(importlist17))});`,document.getElementsByTagName('head')[0].appendChild(src)); while (importlist[imports + 1])
+      })();
+    })();
+  })();
+})();
